efb47f26cdb24af2a61f3738ce3f22c15ed6ac97
Ben Klopfenstein <benklop@gmail.com>
2018-07-21 22:59:56 -0600

    simplify, deal with long/double press on pi

--- RadioKnobs.ino
+++ RadioKnobs.ino
@@ -1,5 +1,3 @@
-#include <TimerOne.h>
-#include <TimerThree.h>
 #include <PinChangeInterrupt.h>
 #include <PinChangeInterruptBoards.h>
 #include <PinChangeInterruptPins.h>
@@ -16,102 +14,25 @@
 #define RIGHT_KNOB_B 0
 #define RIGHT_KNOB_BUTTON 9
 
-#define UNPRESSED 0
-#define SHORT_PRESS 1
-#define LONG_PRESS 2
-#define DOUBLE_PRESS 3
-
-#define DOUBLE_PRESS_MILLIS 250
-#define LONG_PRESS_MILLIS 500
-
-volatile unsigned long left_release_time, right_release_time = 0;
-
-volatile bool left_long_pressed, right_long_pressed = false;
-volatile int left_button, right_button = UNPRESSED;
-
-void leftButtonShortPressTimer() {
-  Timer1.stop();
-  left_button = SHORT_PRESS;
-}
-
-void leftButtonLongPressTimer() {
-  Timer1.stop();
-  left_long_pressed = true;
-  left_button = LONG_PRESS;
-}
-
 void leftButtonInterrupt() {
   uint8_t trigger = getPCINTTrigger(digitalPinToPCINT(LEFT_KNOB_BUTTON));
-  left_long_pressed = false;
+
   if (trigger == FALLING) {
-    unsigned long left_release_interval;
-    if (left_release_time > millis()) {//there was a rollover
-      left_release_interval = (4294967295L - left_release_time) + millis();
-    } else {
-      left_release_interval = millis() - left_release_time;
-    }
-    
-    if (left_release_interval < DOUBLE_PRESS_MILLIS) {
-      left_button = DOUBLE_PRESS;
-    }
-    else {
-      Timer1.stop();
-      Timer1.setPeriod(500000);
-      Timer1.attachInterrupt(leftButtonLongPressTimer);
-      Timer1.restart();
-    }
+    Keyboard.press(KEY_F1);
   }
   else  if (trigger == RISING) {
-    left_release_time = millis();
-    if (! left_long_pressed) {
-      Timer1.stop();
-      Timer1.setPeriod(250000);
-      Timer1.attachInterrupt(leftButtonShortPressTimer);
-      Timer1.restart();
-    }
+    Keyboard.release(KEY_F1);
   }    
-}
-
-void rightButtonShortPressTimer() {
-  Timer1.stop();
-  right_button = SHORT_PRESS;
-}
-
-void rightButtonLongPressTimer() {
-  Timer1.stop();
-  right_long_pressed = true;
-  right_button = LONG_PRESS;
 }
 
 void rightButtonInterrupt() {
   uint8_t trigger = getPCINTTrigger(digitalPinToPCINT(RIGHT_KNOB_BUTTON));
 
   if (trigger == FALLING) {
-    unsigned long right_release_interval;
-    if (right_release_time > millis()) {//there was a rollover
-      right_release_interval = (4294967295L - right_release_time) + millis();
-    } else {
-      right_release_interval = millis() - right_release_time;
-    }
-    
-    if (right_release_interval < DOUBLE_PRESS_MILLIS) {
-      right_button = DOUBLE_PRESS;
-    }
-    else {
-      Timer1.stop();
-      Timer1.setPeriod(LONG_PRESS_MILLIS * 1000L);
-      Timer1.attachInterrupt(rightButtonLongPressTimer);
-      Timer1.restart();
-    }
+    Keyboard.press(KEY_F2);
   }
   else  if (trigger == RISING) {
-    right_release_time = millis();
-    if (! right_long_pressed) {
-      Timer1.stop();
-      Timer1.setPeriod(DOUBLE_PRESS_MILLIS * 1000L);
-      Timer1.attachInterrupt(rightButtonShortPressTimer);
-      Timer1.restart();
-    }
+    Keyboard.release(KEY_F2);
   }    
 }
 
@@ -180,39 +101,5 @@ void loop() {
       #endif
     }
     knobRight.write(0);
-  }
-
-  switch(left_button) {
-    case SHORT_PRESS:
-      Keyboard.write(KEY_F1);
-      left_button = UNPRESSED;
-      break;
-
-    case LONG_PRESS:
-      Keyboard.write(KEY_F2);
-      left_button = UNPRESSED;
-      break;
-
-    case DOUBLE_PRESS:
-      Keyboard.write(KEY_F3);
-      left_button = UNPRESSED;
-      break;
-  }
-
-  switch(right_button) {
-    case SHORT_PRESS:
-      Keyboard.write(KEY_F4);
-      right_button = UNPRESSED;
-      break;
-
-    case LONG_PRESS:
-      Keyboard.write(KEY_F5);
-      right_button = UNPRESSED;
-      break;
-
-    case DOUBLE_PRESS:
-      Keyboard.write(KEY_F6);
-      right_button = UNPRESSED;
-      break;
   }
 }
